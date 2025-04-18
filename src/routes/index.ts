e333c7693210b0f5061ebfe1d7b8d038f126920e
Obkeldiyev <obkeldiyevgiga13@gmail.com>
2025-04-18 19:49:14 +0500

    fuck

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import adminRoutes from "./admin";
 import wishRoutes from "./wishes";
 import noteRoutes from "./notes";
-import { mainPage } from "src/controllers/main.page.controller";
+import { mainPage } from "../controllers/main.page.controller";
 import diaryRoutes from "./diary";
 import chatRoutes from "./chat";
 
