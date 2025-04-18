da8dacd2ef49c556c2e500ff272de5f06c6bf606
Obkeldiyev <obkeldiyevgiga13@gmail.com>
2025-04-18 19:47:50 +0500

    error

--- src/middlewares/errorHandler.middleware.ts
+++ src/middlewares/errorHandler.middleware.ts
@@ -1,4 +1,4 @@
-import { ErrorHandler } from "@errors";
+import { ErrorHandler } from "../errors/errorHandler";
 import { NextFunction, Request, Response } from "express";
 
 export class ErrorHandlerMiddleware {
